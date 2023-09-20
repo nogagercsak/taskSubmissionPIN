8cf40171127b8f97cbf2ff9a47774c862c9538ac
Noga <nogah0317@gmail.com>
2023-09-20 11:28:16 -0400

    Initial Commit

---
+++ taskSubmissionPIN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  taskSubmissionPIN
+//
+//  Created by Noga Gercsak on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ taskSubmissionPIN/taskSubmissionPINApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskSubmissionPINApp.swift
+//  taskSubmissionPIN
+//
+//  Created by Noga Gercsak on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct taskSubmissionPINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
